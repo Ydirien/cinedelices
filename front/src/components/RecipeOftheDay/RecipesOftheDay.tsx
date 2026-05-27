0357159094da17e50c73769065917712e167ebe3
Rambeloson Angelo <angelorambeloson@gmail.com>
2026-05-27 16:09:09 +0200

    size fix home page | antonio

---
+++ front/src/components/RecipeOftheDay/RecipesOftheDay.tsx
@@ -0,0 +1,18 @@
+function recipesOftheDay(){
+    return(
+        <section className="RecipeOfTheDay">
+            <div className="Recipe-Info">
+                <h1>Recette du jour</h1>
+                <h2>Polpette à la sauce tomate - Le Parrain </h2>
+                <p>Dans la scène culte, Peter Clemenza enseigne la recette à Michael 
+                    Corleone avant de lui dire 'Laisse le flingue, prends les cannolis'.
+                </p> 
+            </div>
+            <div className="Recipe-img">
+                <img src="../../../public/Logo/LOGO_pricipal_allonger.png" alt="CinéDélices Logo" className="RecipeOfTheDayImg" />
+            </div>
+        </section>
+    );
+}
+
+export default recipesOftheDay;
--- front/src/pages/homePage/HomePage.tsx
+++ front/src/pages/homePage/HomePage.tsx
@@ -1,24 +1,14 @@
 import CategoriesButton from '../../components/categories/categoriesButton/CategoriesButton';
 import RecoHomePage from '../../components/Recettes_cards/recommandation/HomePageReco';
+import RecipesOftheDay from '../../components/RecipeOftheDay/RecipesOftheDay';
 import './homePage.css';
 function HomePage(){
   return(
     <main>
-      <section className="RecipeOfTheDay">
-        <div className="Recipe-Info">
-          <h1>Recette du jour</h1>
-          <h2>Polpette à la sauce tomate - Le Parrain </h2>
-          <p>Dans la scène culte, Peter Clemenza enseigne la recette à Michael 
-              Corleone avant de lui dire 'Laisse le flingue, prends les cannolis'.
-          </p> 
-        </div>
-        <div className="Recipe-img">
-          <img src="../../../public/Logo/LOGO_pricipal_allonger.png" alt="CinéDélices Logo" className="logo" />
-        </div>
-      </section>
+      <RecipesOftheDay/>
       <CategoriesButton/>
       <RecoHomePage/>
-      </main>
-)}
+    </main>
+);}
 
 export default HomePage

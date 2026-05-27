376d344618855f70392bd6c4517c0318034cf6bd
Rambeloson Angelo <angelorambeloson@gmail.com>
2026-05-27 13:22:45 +0200

    section menu du jours Angelo

--- front/src/pages/homePage/HomePage.tsx
+++ front/src/pages/homePage/HomePage.tsx
@@ -1,8 +1,19 @@
-
+import './homePage.css';
 function HomePage(){
   return(
     <main>
-      
+      <section className="RecipeOfTheDay">
+        <div className="Recipe-Info">
+          <h1>Recette du jour</h1>
+          <h2>Polpette à la sauce tomate - Le Parrain </h2>
+          <p>Dans la scène culte, Peter Clemenza enseigne la recette à Michael 
+            Corleone avant de lui dire 'Laisse le flingue, prends les cannolis'.
+          </p> 
+        </div>
+        <div className="Recipe-img">
+        <img src="../../../public/Logo/LOGO_pricipal_allonger.png" alt="CinéDélices Logo" className="logo" />
+      </div>
+      </section>
     </main>
   )
 }

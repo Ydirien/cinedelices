eaa3b0115d59aa09c24212d7be64d99d2a71b226
Rambeloson Angelo <angelorambeloson@gmail.com>
2026-05-27 10:28:46 +0200

    header - logo - hover

--- front/src/components/header/Header.tsx
+++ front/src/components/header/Header.tsx
@@ -1,10 +1,11 @@
-import './Header.css';
+import './header.css';
+import {NavLink} from 'react-router-dom';
 
 function Header() {
   return (
     <header className="Header">
       <div className="navbar-brand">
-        <img src="logo.png" alt="CinéDélices Logo" className="logo" />
+        <img src="../public/Logo/LOGO.png" alt="CinéDélices Logo" className="logo" />
       </div>
 
       <nav className="navbar-links">

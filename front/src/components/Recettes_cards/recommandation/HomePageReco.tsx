224ba9c8a3af4bece882955f40f3839a251fe072
Rambeloson Angelo <angelorambeloson@gmail.com>
2026-05-27 17:16:37 +0200

    recommandation

--- front/src/components/Recettes_cards/recommandation/HomePageReco.tsx
+++ front/src/components/Recettes_cards/recommandation/HomePageReco.tsx
@@ -4,17 +4,43 @@ function RecoHomePage() {
   return (
     <section className="reco-home-page">
         <h2>Recommandations</h2>
-        <div className="Card">
+        <div className="reco-container">
+            <div className="Card">
             <NavLink to="/#">
                 <div className="RecipeIMG">
                     <img src="../../../public/Logo/LOGO_pricipal_allonger.png" alt="image de la recette" className="recetteImage" />
                 </div>
                 <div className="Recipe-Info">
                     <h3>Cannolis siciliens</h3>
-                    <h4>Le parain</h4>
+                    <h4>Le parain</h4>2;
                     <p>■■■■□ 4/5 - 30min - facile</p>
                 </div>
             </NavLink>
+            </div>
+            <div className="Card">
+            <NavLink to="/#">
+                <div className="RecipeIMG">
+                    <img src="../../../public/Logo/LOGO_pricipal_allonger.png" alt="image de la recette" className="recetteImage" />
+                </div>
+                <div className="Recipe-Info">
+                    <h3>Cannolis siciliens</h3>
+                    <h4>Le parain</h4>2;
+                    <p>■■■■□ 4/5 - 30min - facile</p>
+                </div>
+            </NavLink>
+            </div>
+            <div className="Card">
+            <NavLink to="/#">
+                <div className="RecipeIMG">
+                    <img src="../../../public/Logo/LOGO_pricipal_allonger.png" alt="image de la recette" className="recetteImage" />
+                </div>
+                <div className="Recipe-Info">
+                    <h3>Cannolis siciliens</h3>
+                    <h4>Le parain</h4>2;
+                    <p>■■■■□ 4/5 - 30min - facile</p>
+                </div>
+            </NavLink>
+            </div>
         </div>
     </section>
   );

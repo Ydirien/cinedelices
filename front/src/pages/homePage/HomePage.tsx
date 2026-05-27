650e801732ab444a690bbee2c64cb6de2c55dc93
Rambeloson Angelo <angelorambeloson@gmail.com>
2026-05-27 15:21:02 +0200

    home page reco | wranito

--- front/src/pages/homePage/HomePage.tsx
+++ front/src/pages/homePage/HomePage.tsx
@@ -1,4 +1,5 @@
 import CategoriesButton from '../../components/categories/categoriesButton/CategoriesButton';
+import RecoHomePage from '../../components/Recettes_cards/recommandation/HomePageReco';
 import './homePage.css';
 function HomePage(){
   return(
@@ -8,14 +9,15 @@ function HomePage(){
           <h1>Recette du jour</h1>
           <h2>Polpette à la sauce tomate - Le Parrain </h2>
           <p>Dans la scène culte, Peter Clemenza enseigne la recette à Michael 
-            Corleone avant de lui dire 'Laisse le flingue, prends les cannolis'.
+              Corleone avant de lui dire 'Laisse le flingue, prends les cannolis'.
           </p> 
         </div>
         <div className="Recipe-img">
-        <img src="../../../public/Logo/LOGO_pricipal_allonger.png" alt="CinéDélices Logo" className="logo" />
-      </div>
+          <img src="../../../public/Logo/LOGO_pricipal_allonger.png" alt="CinéDélices Logo" className="logo" />
+        </div>
       </section>
       <CategoriesButton/>
+      <RecoHomePage/>
       </main>
 )}
 

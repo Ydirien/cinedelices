bab8e238d74b29b848864424b9ac5d855416d476
Rambeloson Angelo <angelorambeloson@gmail.com>
2026-05-28 17:50:39 +0200

    feat: mobile responsive v1 (home page)

--- front/src/components/header/Header.tsx
+++ front/src/components/header/Header.tsx
@@ -1,13 +1,18 @@
 import { NavLink } from 'react-router-dom';
+import { LuCircleUser } from 'react-icons/lu';
+import { LuSearch } from 'react-icons/lu';
+import { LuMenu } from "react-icons/lu";
 import './Header.css';
 
 function Header() {
   return (
     <header className="Header">
-      <div className="navbar-brand">
+      <button className='burger-menu'>
+        <LuMenu size={25} />
+      </button>
+      <NavLink to="/" className="navbar-brand">
         <img src="../../../public/Logo/LOGO_pricipal_allonger.png" alt="CinéDélices Logo" className="logo" />
-      </div>
-
+      </NavLink>
       <nav className="navbar-links">
         <NavLink to="/">Accueil</NavLink>
         <NavLink to="/recettes">Recettes</NavLink>

69892f6d7d0ee17c88cf1e60882eda3e360a9a20
JordanP974 <paris.jordan@proton.me>
2026-05-29 15:33:39 +0200

    Add filter bar to the recipes page and update the application structure

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,16 +3,19 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer'
 import HomePage from './pages/homePage/HomePage';
 import { Route, Routes } from 'react-router-dom';
+import RecipesPage from './pages/RecipesPage/RecipesPage';
+import { useState } from 'react';
 
 
 function App() {
-
+  
   return (
     <>
       <Header />
       <main>
       <Routes>
         <Route path='/' element={<HomePage/>} />
+        <Route path='/recettes' element={<RecipesPage/>}/>
       </Routes>
       </main>
       <Footer />

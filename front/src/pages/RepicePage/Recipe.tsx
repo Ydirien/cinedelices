786cf359068adaefe793a5c1c9a644f3a33c053e
Khepri <angelorambeloson@gmail.com>
2026-06-01 14:42:03 +0200

    recipePage v2

--- front/src/pages/RepicePage/Recipe.tsx
+++ front/src/pages/RepicePage/Recipe.tsx
@@ -1,13 +1,17 @@
 import "./Recipe.css"
+import { useState } from "react";
 function RecipePage() {
+   const [NavSwitch, SetNav] = useState(1);
+
   return (
-   <div>
+   <div className="RecipePage">
       <section className="RecipeImage">
-          <img src="/Logo/LOGO_pricipal_allonger.png" alt="" />
+          <img src="/recipes-picture/Bœuf bowl façon Food Wars.png" alt="" />
       </section>
       <section className="Info">
         <h2>Nom de la recette</h2>
         <h4>Film associé</h4>
+        <div className="Stars">★★★★☆ (4/5)</div>
         <p>
           Dans la scène culte, Peter Clemenza enseigne la recette à Michael Corleone avant de lui dire :
           "Laisse le flingue, prends les cannolis".
@@ -17,12 +21,10 @@ function RecipePage() {
             <img src="/icons/time.svg" alt="" />
             <span>20 min</span>
         </li>
-
         <li>
             <img src="/icons/level.svg" alt="" />
             <span>Facile</span>
         </li>
-
         <li>
             <img src="/icons/movie.svg" alt="" />
             <span>Film</span>
@@ -35,25 +37,35 @@ function RecipePage() {
         </ul>
       </section>
       <section className="Nav">
-        <button>Ingrédients</button>
-        <button>Steps</button>
-        <button>Avis</button>
+        <button onClick={()=> SetNav(1)} style={{color: NavSwitch == 1? 'var' : 'var(--tertiary-color)'}}>Ingrédients</button>
+        <button onClick={()=> SetNav(2)}>Steps</button>
+        <button onClick={()=> SetNav(3)}>Avis</button>
       </section>
-      <section className="Ingredients">
-         <ul>
-          <li>500g de viande hachée mixte (bœuf + porc)</li>
-          <li>500g de viande hachée mixte (bœuf + porc)</li>
-          <li>500g de viande hachée mixte (bœuf + porc)</li>
-          <li>500g de viande hachée mixte (bœuf + porc)</li>
-          <li>500g de viande hachée mixte (bœuf + porc)</li>
-          <li>500g de viande hachée mixte (bœuf + porc)</li>
-          <li>500g de viande hachée mixte (bœuf + porc)</li>
-          <li>500g de viande hachée mixte (bœuf + porc)</li>
-          <li>500g de viande hachée mixte (bœuf + porc)</li>
+      {NavSwitch == 1 && (
+        <section className="Ingredients">
+        <div className="List">
+          <ul>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
+            <li>500g de viande hachée mixte (bœuf + porc)</li>
          </ul>
+        </div>
       </section>
-
+      )}
+      {NavSwitch == 2 && (
+        
       <section className="Steps">
+        <div className="List">
           <ul>
             <li>Mélanger la viande, les œufs, le parmesan</li>
             <li>Mélanger la viande, les œufs, le parmesan</li>
@@ -61,26 +73,65 @@ function RecipePage() {
             <li>Mélanger la viande, les œufs, le parmesan</li>
             <li>Mélanger la viande, les œufs, le parmesan</li>
             <li>Mélanger la viande, les œufs, le parmesan</li>
+            <li>Mélanger la viande, les œufs, le parmesan</li>
+            <li>Mélanger la viande, les œufs, le parmesan</li>
+            <li>Mélanger la viande, les œufs, le parmesan</li>
+            <li>Mélanger la viande, les œufs, le parmesan</li>
+            <li>Mélanger la viande, les œufs, le parmesan</li>
+            <li>Mélanger la viande, les œufs, le parmesan</li>
+            <li>Mélanger la viande, les œufs, le parmesan</li>
+            <li>Mélanger la viande, les œufs, le parmesan</li>
+            <li>Mélanger la viande, les œufs, le parmesan</li>
          </ul>
+        </div>
       </section>
-      <section className="Comments">
-        <article className="Comment">
-          <div className="CommentHeader">
-            <img src="/icons/user.svg" alt="" />
-            <div>
+      )}
+      {NavSwitch == 3 &&(
+         <section className="Comments">
+        <div className="List">
+          <article className="Comment">
+            <div className="CommentHeader">
               <h4>NIB</h4>
               <div className="Stars">★★★★☆</div>
             </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </article>
+          <article className="Comment">
+            <div className="CommentHeader">
+              <h4>NIB</h4>
+              <div className="Stars">★★★★☆</div>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </article>
+          <article className="Comment">
+            <div className="CommentHeader">
+              <h4>NIB</h4>
+              <div className="Stars">★★★★☆</div>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </article>
+          <article className="Comment">
+            <div className="CommentHeader">
+              <h4>NIB</h4>
+              <div className="Stars">★★★★☆</div>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </article>
+          <article className="Comment">
+            <div className="CommentHeader">
+              <h4>NIB</h4>
+              <div className="Stars">★★★★☆</div>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </article>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-        </article>
-          <form className="CommentForm">
-            <input type="text" placeholder="Laissez un commentaire..." className="CommentInput"/>
-            <button type="submit" className="CommentButton">Envoyer</button>
-          </form>
-        </section>
+        <form className="CommentForm">
+          <input type="text" placeholder="Laissez un commentaire..." className="CommentInput"/>
+          <button type="submit" className="CommentButton">Envoyer</button>
+        </form>
+      </section>
+      )}
+     
       <section className="Opinion">
         
       </section>

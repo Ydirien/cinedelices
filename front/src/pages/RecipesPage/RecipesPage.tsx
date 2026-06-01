a6aacc2b9881c8e7c4deff461f9b9d3898c61f43
Khepri <angelorambeloson@gmail.com>
2026-06-01 10:52:00 +0200

    a

--- front/src/pages/RecipesPage/RecipesPage.tsx
+++ front/src/pages/RecipesPage/RecipesPage.tsx
@@ -1,4 +1,3 @@
-
 import RecipesCards from "../../components/Recipes_cards/RecipesPageCards/RecipesPageCards";
 import { useState } from 'react';
 import FilterBar from '../../components/filterBar/FilterBar';
@@ -6,13 +5,15 @@ import './RecipesPage.css';
 
 function RecipesPage() {
   const [recipes, setRecipes] = useState([]);
+
   return (
     <div className="recipes-page">
       <aside className="recipes-sidebar">
         <FilterBar onResults={setRecipes} />
       </aside>
       <div className="recipes-content">
-        {/* grille de recettes */}
+        {/* On passe le tableau de recettes filtrées au composant de cartes */}
+        <RecipesCards recipes={recipes} />
       </div>
     </div>
   );

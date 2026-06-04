49edaef7ec33bd50026f13e0832592beefc603cd
Thibault G. <tous.sur.namek@gmail.com>
2026-06-04 10:03:52 +0200

    retouche style de searchbar

--- front/src/components/SearchBar/SearchBar.tsx
+++ front/src/components/SearchBar/SearchBar.tsx
@@ -3,7 +3,7 @@ import { LuSearch } from 'react-icons/lu';
 export default function SearchBar(){
     return(
         <div className="SearchBar">
-            <input type="seach" placeholder="exemple : Naruto" />
+            <input type="search" placeholder="exemple : Naruto" />
             <LuSearch size={20} />
         </div>
     )

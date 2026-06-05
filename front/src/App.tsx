527d17213b4375374bc244d2081553f3272f4238
Thibault G. <tous.sur.namek@gmail.com>
2026-06-05 12:35:26 +0200

    Ajout page profil user/admin non connectées API

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -17,6 +17,8 @@ import Register from './pages/Auth/register/Register';
 import Login from './pages/Auth/login/login';
 import Passwordlost from './pages/Auth/passwordlost/passwordlost';
 import SearchPage from './pages/SearchPage/SearchPage';
+import UserProfilePage from './pages/ProfilePage/UserProfilePage';
+import AdminProfilePage from './pages/ProfilePage/AdminProfilePage';
 
 function App() {
   const [getAllRecipes, setGetAllrecipes] = useState<IRecipe[]>([]); // recupérer les données de la data recipe en attendants la bdd
@@ -73,6 +75,8 @@ function App() {
           <Route path="/confidentialite" element={<Confidentialite />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/profil" element={<UserProfilePage />} />
+            <Route path="/admin/profil" element={<AdminProfilePage />} />
           <Route path="/passwordlost" element={<Passwordlost />} />
         </Routes>
       </main>

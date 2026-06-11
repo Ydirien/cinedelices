74e96b86b76c2c665c632023efab7d48bc0e15c6
gaëtan plassard <gaetan.plassard@hotmail.fr>
2026-06-11 11:26:00 +0200

    Ajout fonctionnalité modification mot de pase & voir l'état des recettes posté

--- api/src/routes/users.route.ts
+++ api/src/routes/users.route.ts
@@ -6,5 +6,7 @@ export const router = Router();
 
 router.get('/profile', checkRoles(['USER', 'ADMIN']), UsersController.getUserProfile);
 router.put('/profile', checkRoles(['USER', 'ADMIN']), UsersController.updateUserProfile);
+router.patch('/profile/password', checkRoles(['USER', 'ADMIN']), UsersController.changePassword);
 router.delete('/profile', checkRoles(['USER', 'ADMIN']), UsersController.deleteAccount);
+router.get('/profile/recipes', checkRoles(['USER', 'ADMIN']), UsersController.getOwnRecipes);
 router.get('/profile/likes', checkRoles(['USER', 'ADMIN']), UsersController.getLikedRecipes);
